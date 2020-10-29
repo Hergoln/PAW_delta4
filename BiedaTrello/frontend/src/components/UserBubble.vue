1c0fdf31e4ea119f7de84bc5db784c1e4856efd5
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-10-29 13:02:10 +0100

    Added UserBubble to prepare trello-like control, that displays information about board's members/users

---
+++ BiedaTrello/frontend/src/components/UserBubble.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+export type User = {
+    id: number
+    name: string
+    last_name: string
+}
+
+@Component
+export default class UserBubble extends Vue{
+  @Prop() private user!: User
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
